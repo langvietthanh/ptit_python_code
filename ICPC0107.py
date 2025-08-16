fa6c6c16f43c9a1f77deb9adbf5f0d55e74413ec
langvietthanh <langvietthanh.28072005@gmail.com>
2025-08-17 00:49:56 +0700

    thay đổi input

--- ICPC0107.py
+++ ICPC0107.py
@@ -2,33 +2,6 @@
 a = 0
 b = 0
 
-def sum(s1,s2):
-    n=len(s1)
-    m=len(s2)
-    if(n > m):
-        tmp = s1
-        s1 = s2
-        s2 = tmp
-    elif (s1>s2):
-        tmp = s1
-        s1 = s2
-        s2 = tmp
-    #  s1 < s2
-    k = abs(n-m)
-    s1 = ("0"*k) + s1
-    reversed(s1)
-    reversed(s2)
-    du = 0
-    res = ""
-    for i in range(len(s2)):
-        n1,n2 = int(s1[i]), int(s2[i])
-        num = n1 + n2 + du
-        # print (num)
-        du = num // 10
-        res = str(num%10)+ res
-    if(du):res = str(du) + res
-    return res
-
 def sum_min (s1,s2):
     s3=""
     s4=""
@@ -40,7 +13,7 @@ def sum_min (s1,s2):
         if(item == str(b) ):
             s4 = s4 + str(a)
         else: s4 = s4 + item
-    return sum(s3,s4)
+    return int(s3) + int(s4)
 
 def sum_max (s1,s2):
     s3=""
@@ -53,7 +26,7 @@ def sum_max (s1,s2):
         if(item == str(a) ):
             s4 = s4 + str(b)
         else: s4 = s4 + item
-    return sum(s3,s4)
+    return int(s3) + int(s4)
 
 if __name__ == '__main__':
     n  = int(input())
@@ -63,6 +36,8 @@ if __name__ == '__main__':
             tmp = a
             a=b
             b=tmp
-        s1 = input().strip()
-        s2 = input().strip()
-        print (sum_min(s1,s2),sum_max(s1,s2),sep = "  ")
+        s1 = input()
+        ele = len(s1.split())
+        if(ele >1): s1 ,s2 = s1.split()
+        else : s2 = input()
+        print (sum_min(s1,s2),sum_max(s1,s2),sep = " ")
